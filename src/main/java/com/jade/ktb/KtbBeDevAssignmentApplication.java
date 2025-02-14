563ac6cdddb302fe7feaecefe0a77f97d6bb564c
TAEHYEON <disney1026dlxogus@gmail.com>
2025-02-14 15:54:06 +0900

    Init SpringBoot

---
+++ src/main/java/com/jade/ktb/KtbBeDevAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.jade.ktb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbBeDevAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbBeDevAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jade/ktb/KtbBeDevAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jade.ktb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbBeDevAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
